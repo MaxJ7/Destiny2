bbe440855115d4d844f9bc05b20d57e75e27dfd4
MaxJ7 <Maz2214@outlook.com>
2026-02-07 06:01:34 -0600

    Stable build

--- AgentFiles/.agent/rules/compilemod.md
+++
@@ -1,7 +0,0 @@
----
-trigger: always_on
----
-
-**ALWAYS** compile the mod/effects after finishing a coding task to ensure validity.
-*   **If Shaders were modified**: Run `compile_effects.ps1`.
-*   **If C# was modified**: (If applicable) Run build command or remind user to build.
--- obj/Debug/net8.0/Destiny2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Destiny2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Destiny2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bb9c6802ee3df5005504e7ab3155492683dcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe29fda8bce9b499301d0699fa5254b979ba9916")]
 [assembly: System.Reflection.AssemblyProductAttribute("Destiny2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Destiny2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
